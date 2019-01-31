b5aa2520a4c7e84b0cdcc87c5593cff9b6015495
wravel <wravel@wr.local>
2019-01-31 13:44:45 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clararules "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.cerner/clara-rules "0.19.0"]]
+  :repl-options {:init-ns clararules.core})
---
+++ src/clararules/core.clj
@@ -0,0 +1,9 @@
+(ns clararules.core
+  (:require [clara.rules :refer :all]))
+
+
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
